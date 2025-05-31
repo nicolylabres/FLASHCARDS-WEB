e45bda68f5660a5965ff9a5252fbe8045f7ec973
nicolylabres <nicoly.labres.souza@escola.pr.gov.br>
2025-05-31 02:11:23 +0000

    começando js

---
+++ assets/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual linha imaginária divide a Terra em hemisfério norte e sul?',
+    'Linha do Equador'
+)
+
+criaCartao(
+    'História',
+    'Em que ano ocorreu a Proclamação da República no Brasil?',
+    '1889'
+)
+
+criaCartao(
+    'Biologia',
+    ' Qual é a organela responsável pela produção de energia nas células?',
+    'Mitocôndria'
+)
+
+criaCartao(
+    'inglês',
+    'Como é tchau em inglês?', 
+    'Bye'
+)
